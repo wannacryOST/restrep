39e0ded172667268eb4b1bf4f591d292b07abeed
atillaost <atilla.gueven@ost.ch>
2021-11-15 18:21:08 +0100

    Strukturieren

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -2,14 +2,15 @@ var express=require('express');
 var router=express.Router();
 var chatHistory=[];
 var nicknames=[];
+
 router.get('/',function(req,res,next){
 res.json({message:'fhschat-appapiworks...'});
 });
+
 router.get('/history',function(req,res,next){
 res.send(chatHistory);
 });
 
-// hallo
 router.post('/history',function(req,res,next){
 var date= new Date();
 chatHistory.push({message:req.body.message,nickname:req.body.nickname,date:date});
