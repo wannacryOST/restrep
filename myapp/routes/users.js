2a6456f293cd5584e1587b8e4e0049b4cf1ebc5a
atillaost <atilla.gueven@ost.ch>
2021-11-15 17:00:11 +0100

    express

---
+++ myapp/routes/users.js
@@ -0,0 +1,16 @@
+var express=require('express');
+var router=express.Router();
+var chatHistory=[];
+var nicknames=[];
+router.get('/',function(req,res,next){
+res.json({message:'fhschat-appapiworks...'});
+});
+router.get('/history',function(req,res,next){
+res.send(chatHistory);
+});
+router.post('/history',function(req,res,next){
+var date= new Date();
+chatHistory.push({message:req.body.message,nickname:req.body.nickname,date:date});
+res.json({message:'Historycreated!'});
+});
+module.exports=router;
